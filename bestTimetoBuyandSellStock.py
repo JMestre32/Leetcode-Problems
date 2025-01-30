12bf112881b6cbe07a2c91755df91ed5a2d09671
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2025-01-30 13:55:58 -0800

    comments

--- bestTimetoBuyandSellStock.py
+++ bestTimetoBuyandSellStock.py
@@ -17,3 +17,13 @@ class Solution:
             right += 1
         
         return maxP
+
+#Topics:
+
+# Two Pointer
+
+# Conceptual Answer
+
+# You want to have two pointers, left and right, to hold the index of the most profitable day essentially. So, when the left pointer is less than the right, use a profit variable
+# to compare profit to maxP (which is initialized to 0). If this never happens, then right is incremented at each loop until no profit is found and 0 is returned as maxP, meaning
+# there is no way to profit given the array. 
